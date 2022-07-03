821de43165ca14a92a8a516cdf5161ebac1822e3
Nathan Gaede <nathangaede@Nathans-MacBook-Pro.local>
2022-07-03 13:19:17 -0500

    Created addToCollection function with a return of addAlbum

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,36 @@
 console.log('***** Music Collection *****')
 
 //Create variable "collection" as an empty array.
-const collection = [];
+const collection = [ 
+    
+]
+
 //Testing for empty array.
 console.log(collection);
+
+//Create function "addToCollection". Album title, artist, yearPublished are the 
+//input parameters (string).
+
+/**
+ * 
+ * @param {string} title 
+ * @param {string} artist 
+ * @param {string} yearPublished 
+ * @returns 
+ */
+
+function addToCollection( title, artist, yearPublished ) {
+    const addAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+        
+        };
+collection.push(addAlbum);
+return addAlbum;
+}
+addToCollection( 'Dirt', 'Alice in Chains', '1992');
+
+console.log(addToCollection('Dirt', 'Alice in Chains', '1992'))
+
+
